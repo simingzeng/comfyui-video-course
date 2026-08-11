3e7b7f3a78560eb4e5dc84eac077785a55515f91
simingzeng <simingzeng@gmail.com>
2026-08-12 05:06:40 +0800

    style: add 3D hero cover, mermaid, glassmorphism theme

--- docs/.vitepress/dist/assets/chunks/swimlanesDiagram-ULZ7WXOC.Dg8HbF5m.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.CvtuzMas.js";import{_ as a}from"../app.BJpMFyKs.js";import"./chunk-5VM5RSS4.D7YYF6nB.js";import"./chunk-XXDRQBXY.DJDUtVZ8.js";import"./chunk-KBJHAD2P.YVGKzDbx.js";import"./chunk-2GRJ4B5K.1ccxL2-W.js";import"./channel._jFc6BG6.js";import"./framework.D-eu8Ik3.js";import"./theme.Bf8ARhCb.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
--- docs/.vitepress/dist/assets/chunks/virtual_mermaid-config.H_WkI8tO.js
+++
@@ -1,1 +0,0 @@
-const e={securityLevel:"loose",startOnLoad:!1,theme:"dark"};export{e as default};
